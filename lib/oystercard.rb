1b5b63d2c3dc2badd52062a2830b69d9c8efb6e1
LTurns <lizzieturney@gmail.com>
2020-04-21 16:45:50 +0100

    added minimum balance and touch in/out tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,28 @@
 class Oystercard
   MAXIMUM_BALANCE = 90
-  attr_reader :balance
+  MINIMUM_BALANCE = 1
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = nil
   end
 
   def top_up(value)
     fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + value > MAXIMUM_BALANCE
+    fail "card has insufficient balance to touch-in" if @balance + value < MINIMUM_BALANCE
     @balance += value
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+ def touch_in?
+   @in_journey = true
+ end
+
+  def touch_out?
+    @in_journey  = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,6 @@ describe Oystercard do
     it 'can top up balance' do
       expect { subject.top_up(1) }.to change { subject.balance }.by 1
     end
-  end
 
   it 'raises an error if the maximum balance is exceeded' do
     maximum_balance = Oystercard::MAXIMUM_BALANCE
@@ -18,11 +17,31 @@ describe Oystercard do
     expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
   end
 
+  it 'raises an error if card has insufficient balance to touch-in' do
+    minimum_balance = Oystercard::MINIMUM_BALANCE
+    expect { subject.top_up(0.5) }.to raise_error "card has insufficient balance to touch-in"
+  end
+  end
+
   describe '#deduct' do
-    
+
     it 'deducts an amount from balance' do
-      subject.top_up(20)
       expect { subject.deduct(3) }.to change { subject.balance }.by -3
     end
   end
+
+describe '#touch_in?' do
+  it 'shows if card in journey' do
+    expect { subject.touch_in? }.to change { subject.in_journey }.to true
+  end
 end
+
+describe '#touch_out?' do
+  it 'shows if card in journey ' do
+    expect { subject.touch_out? }.to change { subject.in_journey }.to false
+  end
+ end
+end
+
+#it 'Person is in a journey if card is tapped in' do 
+#  expect { new_card.tap_in }.to change { new_card.in_journey }.from(false).to(true)   end
