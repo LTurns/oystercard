4b874b5182cf6ba803a39c441414b1b6ca23c029
LTurns <lizzieturney@gmail.com>
2020-04-23 18:19:14 +0100

    created a list of journeys hash and test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
+
 class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
   CHARGE = 0.5
-  attr_reader :balance, :in_journey, :entry_station
+  attr_reader :balance, :in_journey, :entry_station, :exit_station, :list_of_journeys
 
   def initialize
     @balance = 0
     @in_journey = false
+    @list_of_journeys = {}
   end
 
   def top_up(value)
@@ -18,13 +20,17 @@ class Oystercard
    fail "Insufficient balance to touch in" if @balance < MINIMUM_BALANCE
    @in_journey = true
    @entry_station = station
+  @list_of_journeys[:entry_station] = station
  end
 
-  def touch_out?
+  def touch_out?(station)
     @in_journey  = false
     deduct(CHARGE)
     @entry_station = nil
+    @exit_station = station
+    @list_of_journeys[:exit_station] = station
   end
+
 
   private
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
-  let(:station) { double }
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
+
+  let(:journey_hash) {{ :entry_station => entry_station, :exit_station => exit_station }}
   #before(:each) #{ subject.top_up(Oystercard::MINIMUM_BALANCE)}
 
   it 'shows balance' do
@@ -27,41 +30,65 @@ describe '#touch_in?' do
   it 'shows if card in journey' do
     minimum_balance = Oystercard::MINIMUM_BALANCE
     subject.top_up(minimum_balance)
-   expect { subject.touch_in?(station) }.to change { subject.in_journey }.to true
+   expect { subject.touch_in?(:entry_station) }.to change { subject.in_journey }.to true
   end
 
   it 'will not touch in if below minimum balance' do
-    subject.top_up(0.5)
-    expect{ subject.touch_in?(station) }.to raise_error "Insufficient balance to touch in"
+    subject.top_up(Oystercard::CHARGE)
+    expect{ subject.touch_in?(:entry_station) }.to raise_error "Insufficient balance to touch in"
   end
 
   it 'should remember the station upon touch_in' do
     subject.top_up(Oystercard::MINIMUM_BALANCE)
-    expect(subject.touch_in? station).to eq station
+    expect(subject.touch_in? :entry_station).to eq :entry_station
   end
 
   it 'should set entry_station to nil on touch_out' do
     subject.top_up(Oystercard::MINIMUM_BALANCE)
-    subject.touch_in?(station)
-    subject.touch_out?
+    subject.touch_in?(:entry_station)
+    subject.touch_out?(:exit_station)
     expect(subject.entry_station).to eq(nil)
+
   end
 
+  it 'stores the hash of entry_station' do
+    subject.top_up(Oystercard::MINIMUM_BALANCE)
+    expect { subject.touch_in? entry_station }.to change { subject.list_of_journeys.length }.by 1
+  end
 end
 
 describe '#touch_out?' do
 
   it 'shows if card in journey ' do
-    subject.touch_out?
+    subject.touch_out?(:exit_station)
     expect(subject.in_journey).to eq false
   end
 
   it 'deducts an amount from balance on touch out' do
-    expect { subject.touch_out? }.to change { subject.balance }.by -Oystercard::CHARGE
+    expect { subject.touch_out?:exit_station }.to change { subject.balance }.by -Oystercard::CHARGE
   end
 
   it 'charges journey on touch_out method' do
-    expect { subject.touch_out? }.to change { subject.balance }.by -Oystercard::CHARGE
+    expect { subject.touch_out?:exit_station }.to change { subject.balance }.by -Oystercard::CHARGE
+  end
+
+  it 'should set exit_station to true on touch_out' do
+    subject.top_up(Oystercard::MINIMUM_BALANCE)
+    subject.touch_in?:entry_station
+    expect(subject.touch_out? exit_station).to eq exit_station
+  end
+
+  it 'stores the hash of exit station' do
+    subject.top_up(Oystercard::MINIMUM_BALANCE)
+    subject.touch_in? entry_station
+    expect { subject.touch_out? exit_station }.to change { subject.list_of_journeys.length }.by 1
+  end
+
+  it 'checks the journey' do
+    subject.top_up(Oystercard::MINIMUM_BALANCE)
+    subject.touch_in? entry_station
+    subject.touch_out? exit_station
+    expect(subject.list_of_journeys). to eq journey_hash
   end
 end
 end
